6a6a21397d8e9442b276446731515bb6055914d7
fernandorsfernandes <fernandorsfernandes@gmail.com>
2026-04-17 11:40:35 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,6 @@
-const CACHE = 'meu-organizador-v3';
-const ASSETS = [
-  '/meu-organizador/',
-  '/meu-organizador/index.html',
-  '/meu-organizador/manifest.json',
-  '/meu-organizador/icons/icon-192.png',
-  '/meu-organizador/icons/icon-512.png'
-];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        if (!res || res.status !== 200 || res.type === 'opaque') return res;
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }).catch(() => caches.match('/meu-organizador/index.html'));
-    })
-  );
+const CACHE='meu-org-v4';
+const ASSETS=['/meu-organizador/','/meu-organizador/index.html','/meu-organizador/manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>{if(cached)return cached;return fetch(e.request).then(res=>{if(!res||res.status!==200||res.type==='opaque')return res;caches.open(CACHE).then(c=>c.put(e.request,res.clone()));return res;}).catch(()=>caches.match('/meu-organizador/index.html'));}));
 });
